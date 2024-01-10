b4ee70414653b6123890021562df33b5d0c0caab
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-10 12:58:08 -0600

    feature: optimized images

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,3 +1,5 @@
+// Core imports.
+import Image from 'next/image';
 
 // Local imports.
 import styles from './Description.module.sass';
@@ -6,7 +8,14 @@ const Description = (): JSX.Element => {
 
     return (
         <section className = { styles.Description }>
-            <img src="/images/description.jpeg" alt="Description" />
+            <Image 
+                alt="Description" 
+                height = { 300 }
+                src="/images/description.jpeg"
+                width = { 500 }
+                // priority = { false } // Deactivate lazy loading with false.
+                // quality = { 100 }    // Changes the quality of the image, the default is 75 (%).
+            />
             <div className={styles.Description__text}>
                 <h2>Bring the future today</h2>
                 <p>Future World: Your Gateway to Tomorrow&apos;s Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
