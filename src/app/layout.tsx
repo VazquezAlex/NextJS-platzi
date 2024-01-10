1e4949aa404ed85ba8a58899718238dcbb8c7d72
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-10 13:11:56 -0600

    feature: updated font family on project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
-// Third-party imports.
-import { Inter } from 'next/font/google'
+// Core imports.
+import { Roboto } from 'next/font/google';
 
 // Local imports
 import { Header } from 'app/components/shared/Header';
 import { Footer } from 'app/components/shared/Footer';
 import 'app/sass/globals.sass';
 
-const inter = Inter({ subsets: ['latin'] })
+// Define our font config.
+const roboto = Roboto({
+    weight: ["100", "300", "500", "700"],
+    subsets: ["latin"],
+})
 
 export default function RootLayout({
     children,
@@ -15,7 +19,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className = { roboto.className }>
                 <Header />
                 {children}
                 <Footer />
